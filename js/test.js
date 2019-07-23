8f995695afe839780ebd0302dd401cf159480ab6
e-oncologia <52789797+e-oncologia@users.noreply.github.com>
2019-07-23 08:07:40 +0200

    23julioA

---
+++ js/test.js
@@ -0,0 +1,257 @@
+function mostrar() {
+            div = document.getElementById('flotante');
+            div.style.display = '';
+        }
+
+function mostrarIncorrecto() {
+            div = document.getElementById('flotanteIncorrecto');
+            div.style.display = '';
+        }
+
+
+function cerrar() {
+            div = document.getElementById('flotante');
+            div.style.display = 'none';
+        }
+
+function cerrarIncorrecto() {
+            div = document.getElementById('flotanteIncorrecto');
+            div.style.display = 'none';
+        }
+
+function resultado() {
+        var p1;
+
+        if (document.getElementById('p1').checked==true) {cerrarIncorrecto(), mostrar()}
+        else {cerrar(), mostrarIncorrecto()}
+        }
+
+
+
+
+function mostrarB() {
+        div = document.getElementById('flotanteB');
+        div.style.display = '';
+        }
+
+function mostrarIncorrectoB() {
+        div = document.getElementById('flotanteIncorrectoB');
+        div.style.display = '';
+        }
+
+
+function cerrarB() {
+        div = document.getElementById('flotanteB');
+        div.style.display = 'none';
+        }
+
+function cerrarIncorrectoB() {
+        div = document.getElementById('flotanteIncorrectoB');
+        div.style.display = 'none';
+        }
+
+function resultadoB() {
+        var b1;
+
+        if (document.getElementById('b1').checked==true) {cerrarIncorrectoB(), mostrarB()}
+        else {cerrarB(), mostrarIncorrectoB()}
+        }
+
+
+function mostrarC() {
+        div = document.getElementById('flotanteC');
+        div.style.display = '';
+        }
+
+function mostrarIncorrectoC() {
+        div = document.getElementById('flotanteIncorrectoC');
+        div.style.display = '';
+        }
+
+
+function cerrarC() {
+        div = document.getElementById('flotanteC');
+        div.style.display = 'none';
+        }
+
+function cerrarIncorrectoC() {
+        div = document.getElementById('flotanteIncorrectoC');
+        div.style.display = 'none';
+        }
+
+function resultadoC() {
+        var b1;
+
+        if (document.getElementById('b1').checked==true) {cerrarIncorrectoC(), mostrarC()}
+        else {cerrarC(), mostrarIncorrectoC()}
+        }
+
+
+function mostrarD() {
+        div = document.getElementById('flotanteD');
+        div.style.display = '';
+        }
+
+function mostrarIncorrectoD() {
+        div = document.getElementById('flotanteIncorrectoD');
+        div.style.display = '';
+        }
+
+
+function cerrarD() {
+        div = document.getElementById('flotanteD');
+        div.style.display = 'none';
+        }
+
+function cerrarIncorrectoD() {
+        div = document.getElementById('flotanteIncorrectoD');
+        div.style.display = 'none';
+        }
+
+function resultadoD() {
+        var b1;
+
+        if (document.getElementById('b1').checked==true) {cerrarIncorrectoD(), mostrarD()}
+        else {cerrarD(), mostrarIncorrectoD()}
+        }
+
+
+function mostrarE() {
+        div = document.getElementById('flotanteE');
+        div.style.display = '';
+        }
+
+function mostrarIncorrectoE() {
+        div = document.getElementById('flotanteIncorrectoE');
+        div.style.display = '';
+        }
+
+
+function cerrarE() {
+        div = document.getElementById('flotanteE');
+        div.style.display = 'none';
+        }
+
+function cerrarIncorrectoE() {
+        div = document.getElementById('flotanteIncorrectoE');
+        div.style.display = 'none';
+        }
+
+function resultadoE() {
+        var b1;
+
+        if (document.getElementById('b1').checked==true) {cerrarIncorrectoE(), mostrarE()}
+        else {cerrarE(), mostrarIncorrectoE()}
+        }
+
+function mostrarF() {
+        div = document.getElementById('flotanteF');
+        div.style.display = '';
+        }
+
+function mostrarIncorrectoF() {
+        div = document.getElementById('flotanteIncorrectoF');
+        div.style.display = '';
+        }
+
+
+function cerrarF() {
+        div = document.getElementById('flotanteF');
+        div.style.display = 'none';
+        }
+
+function cerrarIncorrectoF() {
+        div = document.getElementById('flotanteIncorrectoF');
+        div.style.display = 'none';
+        }
+
+function resultadoF() {
+        var b1;
+
+        if (document.getElementById('b1').checked==true) {cerrarIncorrectoF(), mostrarF()}
+        else {cerrarF(), mostrarIncorrectoF()}
+        }
+
+function mostrarG() {
+        div = document.getElementById('flotanteG');
+        div.style.display = '';
+        }
+
+function mostrarIncorrectoG() {
+        div = document.getElementById('flotanteIncorrectoG');
+        div.style.display = '';
+        }
+
+
+function cerrarG() {
+        div = document.getElementById('flotanteG');
+        div.style.display = 'none';
+        }
+
+function cerrarIncorrectoG() {
+        div = document.getElementById('flotanteIncorrectoG');
+        div.style.display = 'none';
+        }
+
+function resultadoG() {
+        var b1;
+
+        if (document.getElementById('b1').checked==true) {cerrarIncorrectoG(), mostrarG()}
+        else {cerrarG(), mostrarIncorrectoG()}
+        }
+
+function mostrarH() {
+        div = document.getElementById('flotanteH');
+        div.style.display = '';
+        }
+
+function mostrarIncorrectoH() {
+        div = document.getElementById('flotanteIncorrectoH');
+        div.style.display = '';
+        }
+
+
+function cerrarG() {
+        div = document.getElementById('flotanteH');
+        div.style.display = 'none';
+        }
+
+function cerrarIncorrectoH() {
+        div = document.getElementById('flotanteIncorrectoH');
+        div.style.display = 'none';
+        }
+
+function resultadoH() {
+        var b1;
+
+        if (document.getElementById('b1').checked==true) {cerrarIncorrectoH(), mostrarH()}
+        else {cerrarH(), mostrarIncorrectoH()}
+        }
+
+function mostrarI() {
+        div = document.getElementById('flotanteI');
+        div.style.display = '';
+        }
+
+function mostrarIncorrectoI() {
+        div = document.getElementById('flotanteIncorrectoI');
+        div.style.display = '';
+        }
+
+
+function cerrarI() {
+        div = document.getElementById('flotanteI');
+        div.style.display = 'none';
+        }
+
+function cerrarIncorrectoI() {
+        div = document.getElementById('flotanteIncorrectoI');
+        div.style.display = 'none';
+        }
+
+function resultadoI() {
+        var b1;
+
+        if (document.getElementById('b1').checked==true) {cerrarIncorrectoI(), mostrarI()}
+        else {cerrarI(), mostrarIncorrectoI()}
+        }
